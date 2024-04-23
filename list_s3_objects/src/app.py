00730c795b4d670074228b2e644a9e33205b866b
Feyaaz <feyaazc94@gmail.com>
2024-04-23 16:24:20 +0100

    Apply Formatting

--- list_s3_objects/src/app.py
+++ list_s3_objects/src/app.py
@@ -1,13 +1,13 @@
+import logging
 import os
 from http import HTTPStatus
 
 import boto3
-import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-S3_BUCKET = os.environ['S3_BUCKET']
+S3_BUCKET = os.environ["S3_BUCKET"]
 
 
 def lambda_handler(event, context):
@@ -33,22 +33,26 @@ def lambda_handler(event, context):
     """
 
     try:
-        logger.info(f'Calling out to {S3_BUCKET} bucket to list objects')
-        s3_client = boto3.client('s3')
+        logger.info(f"Calling out to {S3_BUCKET} bucket to list objects")
+        s3_client = boto3.client("s3")
 
         # Get all the objects from the bucket. Max 1000
         objects_in_bucket = s3_client.list_objects(Bucket=S3_BUCKET)
 
-        logger.info('Found {} objects in the bucket. Printing a sample...'.format(len(objects_in_bucket['Contents'])))
+        logger.info(
+            "Found {} objects in the bucket. Printing a sample...".format(
+                len(objects_in_bucket["Contents"])
+            )
+        )
 
-        for key in objects_in_bucket['Contents'][:5]:
-            logger.info('Found {} in bucket'.format(key['Key']))
+        for key in objects_in_bucket["Contents"][:5]:
+            logger.info("Found {} in bucket".format(key["Key"]))
 
     except Exception as e:  # Catch all for easier error tracing in logs
         logger.error(e, exc_info=True)
-        raise Exception('Error occurred during execution')  # notify aws of failure
+        raise Exception("Error occurred during execution")  # notify aws of failure
 
     return {
         "statusCode": HTTPStatus.OK.value,
-        "message": "Successfully Pulled List of S3 Object from Bucket"
+        "message": "Successfully Pulled List of S3 Object from Bucket",
     }
--- list_s3_objects/tests/unit/test_handler.py
+++ list_s3_objects/tests/unit/test_handler.py
@@ -1,71 +1,40 @@
-import json
+from unittest.mock import MagicMock, patch
 
 import pytest
 
-from list_s3_objects.src import app
+from list_s3_objects.src.app import lambda_handler
 
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
-
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
+@patch.dict("os.environ", {"S3_BUCKET": "your_bucket_name"})
+@patch("boto3.client")
+def test_lambda_handler_success(mock_boto3_client):
+    mock_s3_client = MagicMock()
+    mock_boto3_client.return_value = mock_s3_client
+    mock_s3_client.list_objects.return_value = {
+        "Contents": [{"Key": "object1"}, {"Key": "object2"}, {"Key": "object3"}]
     }
 
+    event = {}
+    context = MagicMock()
 
-def test_lambda_handler(apigw_event):
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
+    response = lambda_handler(event, context)
 
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
+    assert response["statusCode"] == 200
+    assert response["message"] == "Successfully Pulled List of S3 Object from Bucket"
+    mock_s3_client.list_objects.assert_called_once_with(Bucket="your_bucket_name")
+
+
+@patch.dict("os.environ", {"S3_BUCKET": "your_bucket_name"})
+@patch("boto3.client")
+def test_lambda_handler_error(mock_boto3_client):
+    mock_s3_client = MagicMock()
+    mock_s3_client.list_objects.side_effect = Exception("Some error")
+    mock_boto3_client.return_value = mock_s3_client
+
+    event = {}
+    context = MagicMock()
+
+    with pytest.raises(Exception) as e:
+        lambda_handler(event, context)
+
+    assert "Error occurred during execution" in str(e.value)
